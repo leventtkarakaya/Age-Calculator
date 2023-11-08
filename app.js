01c4fb13cf7f78d819b15663e0eb64d9036372db
levent <levent=karakaya58@hotmial.com>
2023-11-08 20:10:49 +0300

    Age Calcultor

---
+++ app.js
@@ -0,0 +1,28 @@
+const birthdayEl = document.getElementById("birthday");
+const btnEl = document.getElementById("btn");
+const resultEl = document.getElementById("result");
+
+function AgeCalculator() {
+    const birthdayUser = birthdayEl.value;
+    if (birthdayUser === " ") {
+        alert("Please try again");
+    } else {
+        const age = GetAge(birthdayUser);
+        resultEl.innerText = `Your age is ${age} years old `;
+    }
+}
+
+function GetAge(inputDate) {
+    const currentDate = new Date();
+    const userDate = new Date(inputDate);
+    let age = currentDate.getFullYear() - userDate.getFullYear();
+    const monthDiff = currentDate.getMonth() - userDate.getMonth();
+    const dayDiff = currentDate.getDate() - userDate.getDate();
+
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+        age--;
+    }
+    return age;
+}
+
+btnEl.addEventListener("click", AgeCalculator);
